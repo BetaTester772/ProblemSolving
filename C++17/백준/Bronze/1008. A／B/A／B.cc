1dbd9fe074c49d5273fba16bcd6cc9ce993fd427
Hoseong Ahn <74451807+BetaTester772@users.noreply.github.com>
2025-09-23 22:45:52 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << fixed;
+    cout.precision(9);
+    cout << A / (double) B;
+}
