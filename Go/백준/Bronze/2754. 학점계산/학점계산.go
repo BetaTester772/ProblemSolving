2622aead029496392b58bca953e783bd49d3c3f6
Hoseong Ahn <74451807+BetaTester772@users.noreply.github.com>
2025-09-23 23:18:34 +0900

    [Bronze III] Title: 학점계산, Time: 4 ms, Memory: 892 KB -BaekjoonHub

---
+++ Go/백준/Bronze/2754. 학점계산/학점계산.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s, result string
+	fmt.Scanln(&s)
+
+	switch s {
+	case "A+":
+		result = "4.3"
+	case "A0":
+		result = "4.0"
+	case "A-":
+		result = "3.7"
+	case "B+":
+		result = "3.3"
+	case "B0":
+		result = "3.0"
+	case "B-":
+		result = "2.7"
+	case "C+":
+		result = "2.3"
+	case "C0":
+		result = "2.0"
+	case "C-":
+		result = "1.7"
+	case "D+":
+		result = "1.3"
+	case "D0":
+		result = "1.0"
+	case "D-":
+		result = "0.7"
+	case "F":
+		result = "0.0"
+	}
+	fmt.Println(result)
+}
